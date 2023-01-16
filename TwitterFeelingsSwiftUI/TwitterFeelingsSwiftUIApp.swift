902f7d4e6e33f974fd7331dd8575265ea42f25de
Ordaz Romero Sergio <orcheko@gmail.com>
2023-01-15 19:01:10 -0600

    Initial Commit

---
+++ TwitterFeelingsSwiftUI/TwitterFeelingsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterFeelingsSwiftUIApp.swift
+//  TwitterFeelingsSwiftUI
+//
+//  Created by Sergio Ordaz Romero on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterFeelingsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
